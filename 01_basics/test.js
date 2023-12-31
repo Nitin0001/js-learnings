f5db4d1e4767e05d75b5277480fde3226d09f848
Nitin_Dhiman <cu.16bcs1978@gmail.com>
2023-12-31 15:59:25 +0000

    learnings of object begins

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
-console.log("Hi There!!")
+
+//Object Literals
+const mySymbolTest = Symbol("key1")
+const jsUser = {
+    name: "Nitin Dhiman", 
+    age: 26,
+    [mySymbolTest]: "Lets pray it works"
+}
+jsUser.hobbies = "anime"
+// Object.freeze(jsUser)
+// jsUser.name="ABC"
+jsUser.greetings = function() {
+    console.log(`A very Happy Good Evening Mr. ${this.name}.`)
+}
+// console.log(jsUser.name);
+// console.log(jsUser[mySymbolTest]);
+// console.log(jsUser.hobbies);
+// console.log(jsUser.greetings());
+
+// singleton object
+// const testObj = new Object()
