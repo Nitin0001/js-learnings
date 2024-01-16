efe93fca25dff52c583240f3484f3dfddd10ff54
Nitin_Dhiman <cu.16bcs1978@gmail.com>
2024-01-16 14:27:31 +0000

    for loop learnings

---
+++ 01_basics/for-loops.js
@@ -0,0 +1,8 @@
+const grocery = ['milk', 'maggie', 'paneer', 'red chilli', 'potatos', 'onion']
+
+for (let index = 0; index < grocery.length; index++)
+{
+    console.log(grocery[index])
+    // console.log();
+}
+// console.log(grocery.length);
